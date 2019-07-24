c963a0d54428737fc3f3a487441723a2835638c5
Derek Liu <liuderek97@gmail.com>
2019-07-24 12:20:01 +1000

    Test

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :reviews
   resources :profiles
   devise_for :users
-  # /hello
+  # /hello there
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
