e625e52781f8a6b517e3d58d17ffd5864ae014da
AidanL <aidanlawson31@gmail.com>
2019-07-24 16:20:10 +1000

    basic model validations

--- src/app/models/job.rb
+++ src/app/models/job.rb
@@ -1,3 +1,9 @@
 class Job < ApplicationRecord
   belongs_to :employer, class_name: "Profile", foreign_key: "employer_id"
+  
+  validates_presence_of :amount
+  validates :employer_id, presence: true
+  validates :title,   presence: true
+  validates :content, presence: true
+  validates :address, presence: true
 end
