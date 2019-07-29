dc750020bd2d5bd1c92b1953885176432f39183d
Derek Liu <liuderek97@gmail.com>
2019-07-29 11:20:24 +1000

    Condtional routing

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_profile
   
-  s
+  def after_sign_in_path_for(profile)
+    if current_profile
+      root_path
+    else
+      new_profile_path
+    end
+  end
+ end
+ 
