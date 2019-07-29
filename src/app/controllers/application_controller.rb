920b31dc3a33ee0dddd260a3f7af2bcb407afcf4
AidanL <aidanlawson31@gmail.com>
2019-07-29 11:39:03 +1000

    checkboxes, checK

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   def current_profile
     current_user&.profile
   end
+  helper_method :current_profile
+  
+  def after_sign_in_path_for(profile)
+    if current_profile
+      root_path
+    else
+      new_profile_path
+    end
+  end
 end
