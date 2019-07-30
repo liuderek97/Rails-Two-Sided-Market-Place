4dd3ae14c141449453129c637afe93bd39326b13
Hayden <hayden.fuller@gmail.com>
2019-07-30 13:53:20 +1000

    bucket works, can upload pictures and it stores into bucket.

--- src/app/controllers/profiles_controller.rb
+++ src/app/controllers/profiles_controller.rb
@@ -74,6 +74,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:name, :bio, :bartender, :user_id)
+      params.require(:profile).permit(:name, :bio, :bartender, :user_id, pictures: [])
     end
 end
