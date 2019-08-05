45849de22c07c365fd6c319c0d385e930bc4e88b
AidanL <aidanlawson31@gmail.com>
2019-08-05 15:18:17 +1000

    colour adjust

--- src/app/controllers/bids_controller.rb
+++ src/app/controllers/bids_controller.rb
@@ -29,7 +29,6 @@ class BidsController < ApplicationController
   def create
     @bid = Bid.new(bid_params)
     @job = bid_params[:job_id]
-    puts "BIDPARAMS ALAL#{bid_params}"
     
     respond_to do |format|
       if @bid.save
