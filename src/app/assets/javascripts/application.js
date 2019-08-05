694977b3a3d68ace848ea3047fcd4334835ec088
Hayden <hayden.fuller@gmail.com>
2019-08-05 16:31:54 +1000

    working side nav/ham-nav now

--- src/app/assets/javascripts/application.js
+++ src/app/assets/javascripts/application.js
@@ -18,7 +18,11 @@
 //= require materialize-sprockets
 	
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
 
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
