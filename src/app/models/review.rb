e3746c65d0fab3fbf17a6fe5a85eb402dc3a7296
AidanL <aidanlawson31@gmail.com>
2019-08-09 22:08:15 +1000

    custom validations

--- src/app/models/review.rb
+++ src/app/models/review.rb
@@ -2,11 +2,18 @@ class Review < ApplicationRecord
   belongs_to :reviewer, class_name: "Profile", foreign_key: "reviewer_id"
   belongs_to :reviewed, class_name: "Profile", foreign_key: "reviewed_id"
 
-  validates :star_rating, numericality: { only_integer: true, 
-                                          greater_than_or_equal_to: 1, 
+  validates :star_rating, numericality: { only_integer: true,
+                                          greater_than_or_equal_to: 1,
                                           less_than_or_equal_to:    5 }
   validates :title,       presence: true, length: { maximum: 40 }
   validates :content,     presence: true, length: { maximum: 300 }
   validates :reviewer_id, presence: true
   validates :reviewed_id, presence: true
+  validate  :prohibits_review
+
+  def prohibits_review
+    if reviewer_id == reviewed_id
+      errors.add(:review, "cannot leave a review on your own Profile")
+    end
+  end
 end
