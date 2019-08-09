c42e46a153b020ca9c6a1420cd63528986653793
AidanL <aidanlawson31@gmail.com>
2019-08-09 12:12:19 +1000

    form update

--- src/app/controllers/profiles_controller.rb
+++ src/app/controllers/profiles_controller.rb
@@ -33,14 +33,10 @@ class ProfilesController < ApplicationController
     @profile = Profile.new(profile_params)
     @profile.user_id = current_user.id
 
-    respond_to do |format|
-      if @profile.save
-        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
-        format.json { render :show, status: :created, location: @profile }
-      else
-        format.html { render :new }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
+    if @profile.save
+      redirect_to @profile, notice: 'Profile was successfully created.'
+    else
+      render :new
     end
   end
 
@@ -49,15 +45,12 @@ class ProfilesController < ApplicationController
   def update
     if @profile.pictures
       @profile.pictures.purge
-      end
-    respond_to do |format|
-      if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
-        format.json { render :show, status: :ok, location: @profile }
-      else
-        format.html { render :edit }
-        format.json { render json: @profile.errors, status: :unprocessable_entity }
-      end
+    end
+
+    if @profile.update(profile_params)
+      redirect_to @profile, notice: 'Profile was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -66,8 +59,7 @@ class ProfilesController < ApplicationController
   def destroy
     @profile.destroy
     respond_to do |format|
-      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to jobs_path, notice: 'Profile was successfully destroyed.'
     end
   end
 
